4ac90b4c8d2b1b81990c657365711e8f73776400
LynnnFloyd <90033409+LynnnFloyd@users.noreply.github.com>
2021-09-24 13:59:46 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,56 @@
 input.onGesture(Gesture.Shake, function () {
-    basic.showNumber(randint(-5, 0))
-})
-basic.forever(function () {
-	
+    if (randint(1, 6) == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (randint(1, 6) == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . # .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (randint(1, 6) == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . . . .
+            `)
+    }
+    if (randint(1, 6) == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (randint(1, 6) == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (randint(1, 6) == 6) {
+        basic.showLeds(`
+            . . . . .
+            # . # . #
+            . . . . .
+            # . # . #
+            . . . . .
+            `)
+    }
 })
